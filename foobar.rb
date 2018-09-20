c4015c061bab1cfada6af082699c0b85287074db
angelaliu1 <liuangela555@gmail.com>
2018-09-19 23:51:47 -0700

    basic ruby

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,18 @@ class Foobar
   def self.baz(a)
     # Class method, not an instance method
     # Call with `Foobar.baz`
-    # Q4 CODE HERE
+    x = a.map { |x| x.to_i + 2 }
+    x = x.select { |a| a % 2 == 0 }.uniq
+    sum = 0
+    x.each { |a| sum += a }
+    if sum > 10
+      return 10
+    else
+      return sum
+    end
   end
 end
+
+#x.each do |i|
+#  sum += i
+#end
